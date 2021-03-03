37c74d4c4b724fad89a9285b35d03e0d155efaf5
Seba Kloster <sebikloster@yahoo.com.ar>
2021-03-03 16:55:24 -0300

    Creando la relacion entre entidades

--- IotMetrics-db/index.js
+++ IotMetrics-db/index.js
@@ -1,6 +1,19 @@
 "use strict";
 
-module.exports = function (config) {
+const setupDatabase = require("./lib/db");
+const setupAgentModel = require("./models/agent");
+const setupMetricModel = require("./models/metric");
+
+module.exports = async function (config) {
+  const sequelize = setupDatabase(config);
+  const AgentModel = setupAgentModel(config);
+  const MetricModel = setupMetricModel(config);
+
+  AgentModel.hasMany(MetricModel);
+  MetricModel.belongsTo(AgentModel);
+
+  await sequelize.authenticate(); //Deberia capturar el error de esta promesa, no?
+
   const Agent = {};
   const Metric = {};
 
